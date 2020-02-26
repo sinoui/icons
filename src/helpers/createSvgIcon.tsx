5e07a7a8046e59b641133faadb0e88eacf3ffd42
tianyanqiu <745916729@qq.com>
2020-02-26 19:55:16 +0800

    添加docz依赖

--- src/helpers/createSvgIcon.tsx
+++ src/helpers/createSvgIcon.tsx
@@ -6,7 +6,7 @@ import SvgIcon from '@sinoui/core/SvgIcon';
  * @param path 图标路径
  * @param displayName 图标名称
  */
-export default function createSvgIcon(path: HTMLElement, displayName: string) {
+export default function createSvgIcon(path: any, displayName: string) {
   const Component = React.memo(
     React.forwardRef((props, ref) => (
       <SvgIcon data-testid={`${displayName}Icon`} ref={ref} {...props}>
