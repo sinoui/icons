06ca8c22c9cfad74e36f3ac7369942efc17238af
tianyanqiu <745916729@qq.com>
2021-08-16 17:08:50 +0800

    chore: 升级@sinoui/core版本号为1.0.0

--- src/Brightness1.tsx
+++ src/Brightness1.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
 import createSvgIcon from './helpers/createSvgIcon';
 
-export default createSvgIcon(
-  <circle cx="12" cy="12" r="10" />,
-  'Brightness1',
-);
+export default createSvgIcon(<circle cx="12" cy="12" r="10" />, 'Brightness1');
